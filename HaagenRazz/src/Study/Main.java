40a6ffab07d2ebbc5edcf4ffd34a862c932973f0
HaagenRazz <liminw417@gmail.com>
2022-08-18 22:25:23 +0900

    백준 2441 별 찍기 - 4

--- HaagenRazz/src/Study/Main.java
+++ HaagenRazz/src/Study/Main.java
@@ -8,20 +8,26 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		// https://www.acmicpc.net/problem/2440
+		// https://www.acmicpc.net/problem/2441
 		
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(bf.readLine());
 		
-		for(int i = 0; i < N; i++) {
-			for(int j = i + 1; j <= N; j++) {
+		int N = Integer.parseInt(bf.readLine());
+
+		for (int i = 1; i <= N; i++) {
+
+			for (int k = 0; k <= N - i; k++) {
 				System.out.print("*");
 			}
 			System.out.println();
-			
+
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+
 		}
 		
 		bf.close();
+		
     }
 }
