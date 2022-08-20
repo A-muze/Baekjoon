5153d38381bbbc96f3a77b6f9bed99da83945e78
HaagenRazz <liminw417@gmail.com>
2022-08-20 16:45:52 +0900

    백준 25304 영수증

--- HaagenRazz/src/Study/Main.java
+++ HaagenRazz/src/Study/Main.java
@@ -3,42 +3,41 @@ package Study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
-		// https://www.acmicpc.net/problem/2444
-		
+
+		// https://www.acmicpc.net/problem/25304
+
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(bf.readLine());
-		
-		for(int i = 1; i <= n; i++) {
-			
-			for(int j = 0; j < n - i; j++) {
-				System.out.print(" ");
-			}
-			
-			for(int k = 0; k < 2 * i - 1; k++) {
-				System.out.print("*");
-			}
-			System.out.println();
+
+		int x, n, a, b; // 총 금액, 물건의 수, 물건 가격, 개수
+
+		int sum = 0;
+
+		x = Integer.parseInt(bf.readLine());
+		n = Integer.parseInt(bf.readLine());
+
+		for (int i = 0; i < n; i++) {
+
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+
+			sum += a * b;
+
 		}
-		
-		for(int i = n - 1; i > 0; i--) {
-			
-			for(int j = 0; j < n - i; j++) {
-				System.out.print(" ");
-			} 
-			
-			for(int k = 0; k < i * 2 - 1; k++) {
-				System.out.print("*");
-			}
-			System.out.println();
+		if (x == sum) {
+			System.out.println("Yes");
+
+		} else {
+			System.out.println("No");
 		}
-		
+
 		bf.close();
-		
-    }
+	}
+
 }
