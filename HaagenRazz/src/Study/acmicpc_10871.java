651fece4108b5a06879a4c1d925cedd455d34786
HaagenRazz <liminw417@gmail.com>
2022-08-22 17:06:21 +0900

    백준 10871 X보다 작은 수

---
+++ HaagenRazz/src/Study/acmicpc_10871.java
@@ -0,0 +1,30 @@
+package Study;
+
+import java.util.Scanner;
+
+public class acmicpc_10871 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+
+		int[] A = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
+		}
+
+		sc.close();
+
+		for (int i = 0; i < N; i++) {
+			if (A[i] < X) {
+				System.out.print(A[i] + " ");
+			}
+		}
+		
+	}
+
+}
