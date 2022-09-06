f75f792b12f1ddbb5d966afb53261574b74348bc
HaagenRazz <liminw417@gmail.com>
2022-09-06 13:21:21 +0900

    백준 3052 나머지

---
+++ HaagenRazz/src/Study/acmicpc_3052.java
@@ -0,0 +1,23 @@
+package Study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class acmicpc_3052 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> hash = new HashSet<>();
+
+        for(int i = 0; i < 10; i++) {
+            hash.add(Integer.parseInt(br.readLine()) % 42);
+
+        }
+
+        System.out.print(hash.size());
+
+    }
+}
