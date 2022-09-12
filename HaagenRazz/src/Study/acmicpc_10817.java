3db76dd06af24136c97b1f8883755a237e833581
HaagenRazz <liminw417@gmail.com>
2022-09-13 00:13:52 +0900

    백준 10817 세 수

---
+++ HaagenRazz/src/Study/acmicpc_10817.java
@@ -0,0 +1,25 @@
+package Study;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class acmicpc_10817 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[3];
+
+        for(int i = 0; i < 3; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        Arrays.sort(arr);
+
+        System.out.print(arr[1]);
+
+    }
+}
