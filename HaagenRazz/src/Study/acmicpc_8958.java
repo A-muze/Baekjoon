44bdd0a086b5654b59fa1989b2d07fc76ec95d88
HaagenRazz <liminw417@gmail.com>
2022-09-17 23:14:56 +0900

    백준 8958 OX퀴즈

---
+++ HaagenRazz/src/Study/acmicpc_8958.java
@@ -0,0 +1,37 @@
+package Study;
+
+import java.util.Scanner;
+
+public class acmicpc_8958 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.next();
+        }
+
+        sc.close();
+
+        for(int i = 0; i < arr.length; i++) {
+
+            int count = 0;  // х╫╪Ж
+            int sum = 0;    // гу
+
+            for(int j = 0; j < arr[i].length(); j++) {
+                if(arr[i].charAt(j) == 'O') {
+                    count++;
+                } else {
+                    count = 0;
+                }
+                sum += count;
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
