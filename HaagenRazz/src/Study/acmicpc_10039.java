73192632802cb2cdf5e6b7a8d381de3fc27c9bc5
HaagenRazz <liminw417@gmail.com>
2022-09-22 18:15:39 +0900

    백준 10039 평균 점수

---
+++ HaagenRazz/src/Study/acmicpc_10039.java
@@ -0,0 +1,26 @@
+package Study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class acmicpc_10039 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+
+        for(int i = 0; i < 5; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            if(n < 40) {
+               n = 40;
+            }
+            sum += n;
+
+        }
+        System.out.print(sum / 5);
+    }
+}
